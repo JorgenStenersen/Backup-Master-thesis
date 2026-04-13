bafc258c8e57486ca8288a7c1ffbdca4ddfe8b85
JorgenStenersen <jrstenersen@gmail.com>
2026-04-13 12:17:22 +0200

    Final integration scenario reduction and full model run. ignoring local files.

--- scripts/main.py
+++ scripts/main.py
@@ -9,12 +9,12 @@ import os
 
 if __name__ == "__main__":
 
-    mode = "extensive_form"
-    #mode = "progressive_hedging"
+    #mode = "extensive_form"
+    mode = "progressive_hedging"
 
     path = "./input_data_10.csv"
-    time_str = "2025-06-10 16:00:00+00:00"
-    n = 4
+    time_str = "2025-01-15 16:00:00+00:00"
+    n = 20
     verbose = True
     seed = 30
     #number_of_runs = 20
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     n_per_bundle = 3
     num_bundles = 10
     alpha = 100
-    epsilon = 1
+    epsilon = 2
     adaptive_alpha = True
     tau = 2.0
     mu = 10.0
